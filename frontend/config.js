3aee210f95a93564b233b9a4729baf52e53b0a67
Praveen Reddy <praveenreddy2621@gmail.com>
2025-12-26 14:59:58 +0530

    Update sitema p lastmod date to trigger SEO cra wl

--- frontend/config.js
+++ frontend/config.js
@@ -4,6 +4,3 @@ window.ENV = {
       ? "http://localhost:3001" // local dev
       : "" // docker/production environment (accessed via IP 13.233.139.94)
 };
-// window.ENV = {
-//   BACKEND_API: "http://68.233.116.139:3001"
-// };
